fbca41e9a59d96f74942f00a14502aa87f375991
Arjun <arjunsadhasivan@gmail.com>
2022-01-13 16:17:09 +0530

    third-commit

--- src/Input.js
+++ src/Input.js
@@ -7,19 +7,19 @@ function Input(props) {
 		<div className={props.data ? "form":"noform"}>
 			<form className='form-controls'>
   <div className="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-    <small id="emailHelp" className="form-text">We'll never share your email with anyone else.</small>
+    <label>Email address</label>
+    <input type="email" className="form-control"  aria-describedby="emailHelp" placeholder="Enter email"/>
+    <small  className="form-text">We'll never share your email with anyone else.</small>
   </div>
   <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
+    <label>Password</label>
     <input type="password" className="form-control"  placeholder="Password"/>
   </div>
   <div className="form-group form-check">
     <input type="checkbox" className="form-check" />
-    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+    <label className="form-check-label" >Check me out</label>
   </div>
-  <button type="submit" onClick={props.onLogin} classNameName="btn ">Submit</button>
+  <button type="submit" onClick={props.onLogin} className="btn ">Submit</button>
 </form>
 		</div>
 	)
